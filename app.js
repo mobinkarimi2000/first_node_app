503a9c001f73809465d56160ec63c190e388c19a
mobin home <mobin.codingflutter@gmail.com>
2025-01-07 19:55:21 +0330

    adding body parser

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const http = require('http');
+const express = require("express");
 
-const routes = require('./routes');
+const app = express();
+app.use((req, res, next) => {
+  console.log("help");
+  next(); // this allow the request to the next middleware
+});
+app.use((req, res, next) => {
+  res.send("<h1> Hello form express </h1>");
+});
 
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
-
+const server = http.createServer(app);
 
 server.listen(3000);
